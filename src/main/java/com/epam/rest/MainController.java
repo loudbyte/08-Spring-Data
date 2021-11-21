63158e79e529cb78f0cf6672dd3dc00a287f6b5b
mikhail <davebottomhel@gmail.com>
2021-11-21 14:40:43 +0600

    Add controller

---
+++ src/main/java/com/epam/rest/MainController.java
@@ -0,0 +1,124 @@
+package com.epam.rest;
+
+import com.epam.facade.BookingFacade;
+import com.epam.model.Event;
+import com.epam.model.Ticket;
+import com.epam.model.User;
+import com.epam.model.impl.EventImpl;
+import com.epam.model.impl.UserImpl;
+import java.util.Date;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  private final BookingFacade bookingFacade;
+
+  public MainController(BookingFacade bookingFacade) {
+    this.bookingFacade = bookingFacade;
+  }
+
+  @GetMapping("/event")
+  public Event getEvent(@RequestParam int id) {
+    return bookingFacade.getEventById(id);
+  }
+
+  @GetMapping("/eventByTitle")
+  public List<Event> getEventByTitle(@RequestParam String title) {
+    return bookingFacade.getEventsByTitle(title, 1,1);
+  }
+
+  @GetMapping("/eventByDate")
+  public List<Event> getEventForDay(@RequestParam Date day) {
+    return bookingFacade.getEventsForDay(day, 1,1);
+  }
+
+  @PostMapping("/event")
+  public void createEvent(@RequestParam String title, @RequestParam Date date) {
+    Event event = new EventImpl();
+    event.setTitle(title);
+    event.setDate(date);
+    bookingFacade.createEvent(event);
+  }
+
+  @PutMapping("/event")
+  public void updateEvent(@RequestParam int id, @RequestParam String title, @RequestParam Date date) {
+    Event event = new EventImpl();
+    event.setId(id);
+    event.setTitle(title);
+    event.setDate(date);
+    bookingFacade.updateEvent(event);
+  }
+
+  @DeleteMapping("/event")
+  public void deleteEvent(@RequestParam int id) {
+    bookingFacade.deleteEvent(id);
+  }
+
+
+  @GetMapping("/user")
+  public User getUser(@RequestParam int id) {
+    return bookingFacade.getUserById(id);
+  }
+
+  @GetMapping("/userByName")
+  public List<User> getUserByName(@RequestParam String name) {
+    return bookingFacade.getUsersByName(name, 1,1);
+  }
+
+  @GetMapping("/userByEmail")
+  public User getUserByEmail(@RequestParam String email) {
+    return bookingFacade.getUserByEmail(email);
+  }
+
+  @PostMapping("/user")
+  public void createUser(@RequestParam String name, @RequestParam String email) {
+    User user = new UserImpl();
+    user.setName(name);
+    user.setEmail(email);
+    bookingFacade.createUser(user);
+  }
+
+  @PutMapping("/user")
+  public void updateUser(@RequestParam int id, @RequestParam String name, @RequestParam String email) {
+    User user = new UserImpl();
+    user.setId(id);
+    user.setName(name);
+    user.setEmail(email);
+    bookingFacade.updateUser(user);
+  }
+
+  @DeleteMapping("/user")
+  public void deleteUser(@RequestParam int id) {
+    bookingFacade.deleteUser(id);
+  }
+
+  @PostMapping("/book")
+  public void book(@RequestParam int userId, @RequestParam int eventId,
+      @RequestParam int place, @RequestParam Ticket.Category category) {
+    bookingFacade.bookTicket(userId, eventId,place, category);
+  }
+
+  @GetMapping("/bookedByUser")
+  public List<Ticket> getBookedTicketsByUser(@RequestParam int userId) {
+    User user = bookingFacade.getUserById(userId);
+    return bookingFacade.getBookedTickets(user, 1, 1);
+  }
+
+  @GetMapping("/bookedByEvent")
+  public List<Ticket> getBookedTicketsByEvent(@RequestParam int eventId) {
+    Event event = bookingFacade.getEventById(eventId);
+    return bookingFacade.getBookedTickets(event, 1, 1);
+  }
+
+  @DeleteMapping("/ticket")
+  public void cancel(@RequestParam int id) {
+    bookingFacade.cancelTicket(id);
+  }
+}
